12664f2ad1821cca9b68aaa892635f6155a894a5
DESKTOP-O5J4IM0\Sohyun <chsh981127@naver.com>
2021-03-26 02:02:57 +0900

    [DEV] title scene UI 임시

---
+++ RunAndBuild/Assets/Script/TitleScene.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScene : MonoBehaviour
+{
+    public void OnClickBackground()
+    {
+        Debug.Log("다른 씬으로 이동합니다.");
+    }
+}
