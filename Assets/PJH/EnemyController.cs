9784ffffeaf613f18d7ca55ebef6497825a71279
ouoaz <35379289p@gmail.com>
2021-04-13 10:17:45 +0900

    오크 애니메이션 설정 미완
    캐릭터와 가까워졌을 때 공격 완료

--- Assets/PJH/EnemyController.cs
+++ Assets/PJH/EnemyController.cs
@@ -6,17 +6,30 @@ public class EnemyController : MonoBehaviour
 {
     Rigidbody rb;
     Vector3 dir;
-    float enemySpeed = 15f;
+    float enemySpeed = 25f;
 
     FairyController fc;
+
+    public Animator animator;
+    public bool meetPlayer;
+    public float _elapsedTimeForEnemy = 0f;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
         dir = new Vector3(0f, 0f, -1f);
         transform.Rotate(new Vector3(0f, 180f, 0f));
+        
 
         fc = GameObject.Find("Fairy").GetComponent<FairyController>();
+        meetPlayer = false;
+    }
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetBool("Walk", false);
+        animator.SetBool("Run", true);
     }
 
     private void OnEnable()
@@ -31,7 +44,65 @@ public class EnemyController : MonoBehaviour
 
     private void Update()
     {
+        if (DistanceToPlayer() <= 5f)
+        {
+            meetPlayer = true;
+            animator.SetBool("Run", false);
+        }
+
+        if (!meetPlayer)
+        {
+            MoveToPlayer();
+        }
+        else
+        {
+            AttackToPlayer();
+        }
+    }
+
+    private float DistanceToPlayer()
+    {
+        return Vector3.Distance(transform.position, fc.transform.position);
+    }
+
+    private void MoveToPlayer()
+    {
         transform.LookAt(fc.transform);
         transform.position += dir * enemySpeed * Time.deltaTime;
+    }
+
+    private void AttackToPlayer()
+    {
+        SetEnemyTimer();
+
+        if (GetEnemyTimer() <= 3.5f)
+        {
+            if (transform.name == "Enemy01")
+            {
+                animator.SetBool("Melee Right Attack 01", true);
+            }
+            else if (transform.name == "Enemy02")
+            {
+                animator.SetBool("Melee Right Attack 02", true);
+            }
+            else if (transform.name == "Enemy03")
+            {
+                animator.SetBool("Melee Right Attack 03", true);
+            }
+            else if (transform.name == "Enemy04")
+            {
+                animator.SetBool("Melee Left Attack 01", true);
+            }
+        }
+    }
+
+    float GetEnemyTimer()
+    {
+        return (_elapsedTimeForEnemy += Time.deltaTime);
+    }
+
+    void SetEnemyTimer()
+    {
+        _elapsedTimeForEnemy = 0f;
     }
 }
