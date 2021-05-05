f4ce0d97b93449b563778b8170daf80ce53d16c6
DESKTOP-O5J4IM0\Sohyun <chsh981127@naver.com>
2021-05-06 03:23:46 +0900

    상점 개발 중

---
+++ Assets/Script/Data/PurchaseItemData.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PurchaseItemData
+{
+    public PurchaseItemData(ShopCategory category, int id, string name, int price){
+        this.category = category;
+        this.id = id;
+        this.price = price;
+        this.category = category;
+    }
+    public int id;
+    public string name;
+    public int price;
+    public ShopCategory category;
+}
---
+++ Assets/Script/UI/ShopPurchaseItem.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopPurchaseItem : MonoBehaviour
+{
+    public void SetItem(PurchaseItemData data)
+    {
+        this.itemName.text = data.name;
+        this.price.text = data.price.ToString();
+        this.image.sprite = ImageManager.Instance.GetShopImage(data.id);
+    }
+
+    [SerializeField] private Text itemName;
+    [SerializeField] private Text price;
+    [SerializeField] private Image image;
+}
