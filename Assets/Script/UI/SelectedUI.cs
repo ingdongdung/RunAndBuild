0a1dbfdb0064befc9577ec9263a49b34070ef76d
DESKTOP-O5J4IM0\Sohyun <chsh981127@naver.com>
2021-06-16 03:18:31 +0900

    [DEV] shop

--- Assets/Script/UI/SelectedUI.cs
+++ Assets/Script/UI/SelectedUI.cs
@@ -20,7 +20,16 @@ public class SelectedUI : MonoBehaviour
             DataManager.Instance.PurchaseItem(this.data.Name);
             KingdomScene.Instance.Refresh();
             this.isClose(true);
+
+            this.BuildingAction();
         }
+    }
+    public void OnClickBuilding()
+    {
+        KingdomScene.Instance.Refresh();
+        this.isClose(true);
+        
+        this.BuildingAction();
     }
 
     public void Init(PurchaseItemData item, Sprite sprite, Action<bool> isClose)
@@ -31,6 +40,11 @@ public class SelectedUI : MonoBehaviour
         this.priceText.text = item.Price.ToString();
         this.image.sprite = sprite;
         this.isClose = isClose;
+
+        bool isPurchased = DataManager.Instance.UserData.itemList.Find(l => l.name == this.data.Name) != null;
+        this.buyBtn.SetActive(!isPurchased);
+        this.purchasedBtn.SetActive(isPurchased);
+
         // this.image.sprite = ImageManager.Instance.GetShopImage(item.ID);
     }
 
@@ -38,11 +52,18 @@ public class SelectedUI : MonoBehaviour
     [SerializeField] private Text descText;
     [SerializeField] private Text priceText;
     [SerializeField] private Image image;
+    [SerializeField] private GameObject buyBtn;
+    [SerializeField] private GameObject purchasedBtn;
     private PurchaseItemData data;
     private Action<bool> isClose;
 
     private void UpdateData()
     {
 
+    }
+
+    private void BuildingAction()
+    {
+        // [TODO] 여기에 설치 행동 추가해주세요
     }
 }
