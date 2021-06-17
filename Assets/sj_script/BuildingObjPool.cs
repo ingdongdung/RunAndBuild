16ee7c0aac96622f10536431596dc83c63d7951a
DESKTOP-O5J4IM0\Sohyun <chsh981127@naver.com>
2021-06-17 22:11:40 +0900

    [FIX]

--- Assets/sj_script/BuildingObjPool.cs
+++ Assets/sj_script/BuildingObjPool.cs
@@ -7,9 +7,9 @@ public class BuildingObjPool : MonoBehaviour
     // Start is called before the first frame update
     Queue<GameObject> poolingObjQueue = new Queue<GameObject>();
 
-    
+
     void Start()
-    {    
+    {
     }
 
     public GameObject CreateObject(string findTag)
@@ -18,7 +18,7 @@ public class BuildingObjPool : MonoBehaviour
         Vector3 position = Input.mousePosition;
         Ray ray = Camera.main.ScreenPointToRay(position);
         RaycastHit hit;
-        if(Physics.Raycast(ray, out hit))
+        if (Physics.Raycast(ray, out hit))
         {
             position = hit.transform.position;
         }
@@ -31,14 +31,31 @@ public class BuildingObjPool : MonoBehaviour
         FollowPlayerCamera camera = FindObjectOfType<FollowPlayerCamera>();
         camera.isBuilding = true;
 
+        DataManager.Instance.UserData.buildingList.Add(new BuildingData(findTag, position.x, position.y, position.z));
+        DataManager.Instance.SaveJsonData(DataManager.Instance.UserData);
         return obj;
-    
+
+    }
+
+    public GameObject CreateObject(string name, float x, float y, float z)
+    {
+        Debug.Log("CreateObject");
+        Vector3 position = new Vector3(x, y, z);
+        Quaternion rotation = new Quaternion(0f, 0f, 0f, 1f);
+        GameObject target = GameObject.Find("KingdomBuilding").transform.Find(name).gameObject;
+        GameObject obj = Instantiate(target, position, rotation);
+        obj.SetActive(true);
+        poolingObjQueue.Enqueue(obj);
+
+        DataManager.Instance.UserData.buildingList.Add(new BuildingData(name, position.x, position.y, position.z));
+        return obj;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
         //if (Input.GetKeyDown(KeyCode.F1))
         //{
         //    CreateObject("WaterTower");
